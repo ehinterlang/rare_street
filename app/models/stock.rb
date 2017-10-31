db5f8a4da8037444a55353d9909b7ce841d35b6d
ehinterlang <ed.hinterlang@gmail.com>
2017-10-30 21:02:20 -0400

    Added case insensitive uniqueness constraint.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+  validates :ticker, presence: true, uniqueness: { case_sensitive: false }
+
+  def ticker=(val)
+    write_attribute(:ticker, val.upcase)
+  end
+
 end
