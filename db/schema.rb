891819560dac1517ca0022795099e2c3a918318e
ehinterlang <ed.hinterlang@gmail.com>
2018-01-13 16:00:33 -0500

    Added Setting for persistent configurations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170514174500) do
+ActiveRecord::Schema.define(version: 20180113204620) do
+
+  create_table "settings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "key"
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stocks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "ticker"
