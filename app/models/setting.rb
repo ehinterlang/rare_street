653f9c344ed49fdf7d0c8bc5baeae52df3da4129
ehinterlang <ed.hinterlang@gmail.com>
2018-02-04 16:03:23 -0500

    Added Setting.setup_required_settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,2 +1,20 @@
 class Setting < ApplicationRecord
+  validates :key, presence: true, uniqueness: true
+
+  # From the settings.yml files add all the settings that are required after deleting any settings that are not needed.
+  def self.setup_required_settings
+    Setting.all.each do |setting|
+      setting.destroy unless setting_required?(setting.key)
+    end
+    SETTINGS[:items].each do |required_setting|
+      Setting.create(key: required_setting[:key], value: '') unless Setting.find_by(key: required_setting[:key])
+    end
+    nil
+  end
+
+  private
+
+  def self.setting_required?(setting_value)
+    SETTINGS[:items].any? { |setting| setting[:key] == setting_value }
+  end
 end
--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,3 +1,3 @@
 fail 'Need to set RARE_STREET_DATABASE_USERNAME' unless ENV['RARE_STREET_DATABASE_USERNAME']
 fail 'Need to set RARE_STREET_DATABASE_PASSWORD' unless ENV['RARE_STREET_DATABASE_PASSWORD']
-CONFIG = YAML.load_file("#{Rails.root.to_s}/config/settings.yml")[Rails.env].deep_symbolize_keys
+SETTINGS = YAML.load_file("#{Rails.root.to_s}/config/settings.yml")[Rails.env].deep_symbolize_keys
